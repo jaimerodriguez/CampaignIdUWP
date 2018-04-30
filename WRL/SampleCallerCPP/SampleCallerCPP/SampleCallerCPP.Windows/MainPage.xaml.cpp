73f01592c9265a33ccd9a23e601f6b673af9ba5c
Jaime Rodriguez <jaimer@live.com>
2018-04-30 02:03:58 -0700

    initial checkin

---
+++ WRL/SampleCallerCPP/SampleCallerCPP/SampleCallerCPP.Windows/MainPage.xaml.cpp
@@ -0,0 +1,50 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+#include "ppltasks.h"
+
+using namespace SampleCallerCPP;
+using namespace Concurrency;
+
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+	this->Loaded += ref new RoutedEventHandler( this, &SampleCallerCPP::MainPage::OnLoaded); 
+}
+
+void MainPage::OnLoaded(Object ^unused, RoutedEventArgs^ unused2 )
+{
+
+	WRLSample::CampaignIdHelper^ helper = ref new WRLSample::CampaignIdHelper();
+	create_task(helper->GetCampaignId81Async()).then([this](String^ cid)
+	{
+		String^ message = ref new String( L"CID is: "); 
+		message += cid; 
+
+		this->Result->Text = message; 
+
+	}, task_continuation_context::use_current());
+
+	
+
+}
+
+
---
+++ WRL_Win8x/WRLSample/WRLSample.Shared/CampaignIdHelper.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+
+#include <hstring.h>
+namespace WRLSample
+{
+	public ref class CampaignIdHelper sealed
+	{
+	public:
+		CampaignIdHelper();
+
+		Windows::Foundation::IAsyncOperation<Platform::String^>^ GetCampaignId81Async();
+
+		property int  NetworkRequestTimeOutMills; 
+
+	private:
+ 
+		HRESULT GetCampaignIdFromStoreContextWithWait(HSTRING* pcampaignId );
+		HRESULT GetCampaignIdFromCurrentAppWithWait  (HSTRING* pcampaignId);
+		HRESULT GetCampaignIdFromStoreLicenseWithWait(HSTRING* pcampaignId);
+	}; 
+} 
